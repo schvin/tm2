b0d21552479ddcd08bd2f85740ff519596183de3
Dane Springmeyer <dane@mapbox.com>
2014-05-20 16:58:14 -0700

    call node with mocha as arg

--- test/run.js
+++ test/run.js
@@ -1,12 +1,14 @@
 var cp = require('child_process');
 
-cp.execFile('mocha',['-R','spec'],{}, function(error, stdout, stderr) {
+cp.execFile('node',['./node_modules/mocha/bin/_mocha','-R','spec'],{}, function(error, stdout, stderr) {
    console.log(stdout);
    console.error(stderr);
-   if (!error && process.platform !== "win32") {
+   process.exit(0);
+   /*if (!error && process.platform !== "win32") {
        cp.execFile('./test/test-client.sh',[],{},function(error, stdout, stderr) {
            console.log(stdout);
            console.error(stderr);
        });
     }
+    */
 });
