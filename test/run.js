a31d37efffecd654a5a6663e794501d8360ea751
Dane Springmeyer <dane@mapbox.com>
2014-05-20 16:42:39 -0700

    second try at shelling out

--- test/run.js
+++ test/run.js
@@ -1,24 +1,12 @@
 var cp = require('child_process');
 
-cp.exec('mocha -R spec',function(error, stdout, stderr) {
-   if (error) {
-     console.log(error.stack);
-     console.log('Error code: '+error.code);
-     console.log('Signal received: '+error.signal);
-     process.exit(1);
-   }
+cp.execFile('mocha',['-R','spec'],{}, function(error, stdout, stderr) {
    console.log(stdout);
    console.error(stderr);
-   if (process.platform !== "win32") {
-       cp.exec('.test/test-client.sh',function(error, stdout, stderr) {
-           if (error) {
-             console.log(error.stack);
-             console.log('Error code: '+error.code);
-             console.log('Signal received: '+error.signal);
-             process.exit(1);
-           }
+   if (!error && process.platform !== "win32") {
+       cp.execFile('./test/test-client.sh',[],{},function(error, stdout, stderr) {
            console.log(stdout);
            console.error(stderr);
-       });    
-   }
+       });
+    }
 });
